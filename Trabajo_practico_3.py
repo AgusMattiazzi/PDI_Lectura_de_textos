5b97aa23e1ec20ff16bf7333222d0b5f232460a0
Zuko <agusmattiazzi@gmail.com>
2023-08-10 12:05:32 -0300

    Codigo mas limpio. Ajuste manual del contorno de cada imagen. Pequeñas actualizaciones de codigo

--- Trabajo_practico_3.py
+++ Trabajo_practico_3.py
@@ -3,116 +3,82 @@ import cv2
 import numpy as np
 import matlab       # Funciones homologas a matlab
 
-## --------------------- Comandos basicos de Python -------------------- #
+## ------------------------------- Tratamiento de imagen inicial -------------------------------- #
 # Abrir Imagen
-Img = cv2.imread('foto_1.jpg')
+Img_path = 'foto_7.jpg'
+Img = cv2.imread(Img_path)
 
 # Conversion a escala de grises
 Img_gris = cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
 
 # Umbralado
 # ret,Img_BW1 = cv2.threshold(Img_gris,120,255,cv2.THRESH_BINARY)
-ret,Img_BW1 = cv2.threshold(Img_gris,25,255,cv2.THRESH_OTSU)
-# matlab.imshow(Img_BW1, title = 'Imagen binaria')
+ret,Img_BW = cv2.threshold(Img_gris,25,255,cv2.THRESH_OTSU)
+
 # Los bordes son negros y el fondo (la hoja) es blanco. Para cambiar ésto
-# simplemente se invierte la imagen
-Img_BW1 = cv2.bitwise_not(Img_BW1)  # Negativo
+# se invierte la imagen
+Img_BW = cv2.bitwise_not(Img_BW)  # Negativo
 
-# Limpiar bordes
-Img_BW = matlab.imclearborder(Img_BW1)
-# matlab.imshow(Img_BW, title = 'Sin bordes')
-
-# Dimensiones de la imagen
-Alto,Ancho = reversed(Img_BW.shape[:2])      # Obtengo las dimensiones
-# print("Dimensiones de la imagen: ", Ancho, Alto, "\n")
-
-## --------------------------------------------------------------------- #
-
-## ---------------------------- Dilatacion ----------------------------- #
-# Al principio se ejerce una dilatación pequeña para evitar que las
-# letras tengan sus contornos mal cerrados
+# Dilatacion
+# Al principio se ejerce una dilatación pequeña para evitar que las letras tengan sus contornos
+# mal cerrados
 
 # Para dilatar, hay que crear una estructura llamada kernel
 kernel = np.ones((3,3),np.uint8)
 Img_BW = cv2.dilate(Img_BW, kernel, iterations = 1)
 # El tercer argumento es opcional y por defecto es 1
 
-## --------------------------------------------------------------------- #
+# Limpiar bordes
+Img_BW = matlab.imclearborder(Img_BW)
+matlab.imshow(Img_BW, title = 'Sin bordes')
 
-## ---------------------- Componentes conectadas ----------------------- #
-# img = cv2.imread('Letras y Figuras.tif', cv2.IMREAD_GRAYSCALE)
+# Dimensiones de la imagen
+Alto,Ancho = reversed(Img_BW.shape[:2])      # Obtengo las dimensiones
+# print("Dimensiones de la imagen: ", Ancho, Alto, "\n")
+
+## ---------------------------------------------------------------------------------------------- #
+
+## -------------------------------------- Aperture science -------------------------------------- #
+# Al principio se ejerce una dilatación pequeña para evitar que las
+# letras tengan sus contornos mal cerrados
+
+# Para dilatar, hay que crear una estructura llamada kernel
+kernel = np.ones((3,3),np.uint8)
+Img_BW = cv2.morphologyEx(Img_BW, cv2.MORPH_OPEN, kernel)
+# Img_BW = cv2.dilate(Img_BW, kernel, iterations = 1)
+
+# El tercer argumento es opcional y por defecto es 1
+
+## ---------------------------------------------------------------------------------------------- #
+
+
+## ------------------------------ Alto y ancho promedio de letras ------------------------------- #
 Output = cv2.connectedComponentsWithStats(Img_BW, 8, cv2.CV_32S)
-# https://docs.opencv.org/3.4/d3/dc0/group__imgproc__shape.html#ga107a78bf7cd25dec05fb4dfc5c9e765f
-
-# Resultados
-num_labels =    Output[0]   # Cantidad de elementos
-labels =        Output[1]   # Matriz con etiquetas
-stats =         Output[2]   # Matriz de stats
-centroids =     Output[3]   # Centroides de elementos
-
-# Coloreamos los elementos
-Img_color = matlab.label2rgb(labels,num_labels)
-
-Suma_Alto = 0;    Suma_Ancho = 0
-
-Palabras = Img_BW.copy()
-
-# Agregamos Bounding Box
-for i in range(1,num_labels):
-    Bbox = stats[i,]
-    # Bbox[0] : Coordenada x punto superior izquierdo
-    # Bbox[1] : Coordenada y punto superior izquierdo
-    # Bbox[2] : Ancho
-    # Bbox[3] : Alto
-
-    # Dibujar rectangulos
-    cv2.rectangle(Img_color, (Bbox[0], Bbox[1]), (Bbox[0]+Bbox[2],
-        Bbox[1]+Bbox[3]), (255,255,255), 2)
-    
-    cv2.rectangle(Palabras, (Bbox[0], Bbox[1]), (Bbox[0]+Bbox[2],
-        Bbox[1]+Bbox[3]), (255,255,255), 2)
-
-    Suma_Ancho = Suma_Ancho + Bbox[2]
-    Suma_Alto = Suma_Alto + Bbox[3]
-
-# matlab.imshow(Img_color, title = 'Imagen etiquetada')
-
-# Ancho y alto promedio de letras.
-Ancho_prom = Suma_Ancho/num_labels
-Alto_prom = Suma_Alto/num_labels
+Ancho_prom,Alto_prom = matlab.dim_promedio(Output)
 
 print("Dimensiones promedio de las letras")
 print("Ancho: {:.2f},\tAlto: {:.2f}".format(Ancho_prom, Alto_prom))
 # Se formatean los float para q se muestren sólo dos decimales, una porqueria la verdad.
 
-# matlab.imshow(Img_color, title = 'Matriz Etiqueta RGB')
+## ---------------------------------------------------------------------------------------------- #
 
-# Se usa una dilatación para unir las letras de cada palabra
-Palabras = matlab.imfill(Palabras)
-kernel = np.ones((8,8),np.uint8)    # square (5,5)
-Palabras = cv2.dilate(Palabras, kernel, iterations = 1)
-# matlab.imshow(Palabras, title = 'Palabras 1')  # Parece prometedor
 
-## ------------------------ Deteccion de Texto ------------------------- #
+## ------------------------------------ Deteccion de Texto -------------------------------------- #
 # Se usa dilatación con distintos kernel para resaltar palabras o párrafos
-dil_palabra = 0.3;  dil_texto = 4
+dil_text = 4;  # dil_pal = 0.3
 
-Img_palabra = matlab.dilatacion(Img_BW, Ancho_prom, Alto_prom, dil_palabra)
-# matlab.imshow(Img_palabra, title = 'Palabras detectadas')
+# Img_palabra = matlab.dilatacion_especial(Img_BW, Ancho_prom, Alto_prom, dil_pal)
+# matlab.imshow(Img_palabra, title = 'Palabras detectadas 1')
 
-Img_texto = matlab.dilatacion(Img_BW, Ancho_prom, Alto_prom, dil_texto)
-# matlab.imshow(Img_texto, title = 'Texto detectado')
+Img_texto = matlab.dilatacion_especial(Img_BW, Ancho_prom, Alto_prom, dil_text)
+matlab.imshow(Img_texto, title = 'Texto detectado')
+## ---------------------------------------------------------------------------------------------- #
 
-## ------------------------ Aproximar contorno ------------------------- #
-Img_gris2 = cv2.cvtColor(Img_texto,cv2.COLOR_BGR2GRAY)
 
-# Umbralado
-# ret,Img_BW1 = cv2.threshold(Img_gris,120,255,cv2.THRESH_BINARY)
-ret,Area_Texto = cv2.threshold(Img_gris2,25,255,cv2.THRESH_OTSU)
-Ancho,Alto = Area_Texto.shape
+## ------------------------------------ Aproximar contorno -------------------------------------- #
+Img_color = cv2.cvtColor(Img_BW,cv2.COLOR_GRAY2BGR) # Funciona de B&W a color tambien
 
-Contours, _ = cv2.findContours(Area_Texto,cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
+Contours, _ = cv2.findContours(Img_texto,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in Contours:
     area = cv2.contourArea(cnt)
@@ -123,9 +89,9 @@ for cnt in Contours:
         # Envolvente convexa
         Hull = cv2.convexHull(cnt)
         Hull = np.resize(Hull,( int( np.size(Hull)/2 ), 2))   # Formateo
-        # cv2.drawContours(Img_color,[Hull],0,(255,0,255),5 )
-        # for points in Hull:
-        #     cv2.circle(Img_color,(points[0],points[1]),5,(255,0,0),10)
+        cv2.drawContours(Img_color,[Hull],0,(255,0,255),5 )
+        for points in Hull:
+            cv2.circle(Img_color,(points[0],points[1]),5,(255,0,0),10)
 
         # Minimo Rectangulo contenedor de la figura
         Rect = cv2.minAreaRect(cnt)
@@ -134,76 +100,51 @@ for cnt in Contours:
         # Dibujar Rectangulo de minima area que abarca al texto
         cv2.drawContours(Img_color,[Box],0,(0,0,255),5 )
 
+# matlab.imshow(Img_color, title = 'Imagen a color bla bla')
 # Imprimir resultados para la caja, funca relativamente bien, faltan ajustes
 print(Box, Box.shape)
-# print(Bbox)
-# Hull = np.resize(Hull,(np.int( np.size(Hull)/2 ), 2))
-# print(Hull, Hull.shape )
-# matlab.imshow(Img_color, title = 'Figura')
-# matlab.imshow(Img_parrafo, title = 'Figura')
 
-## --------------------------------------------------------------------- #
+## ---------------------------------------------------------------------------------------------- #
 
-## ------------------------ Adaptar contorno --------------------------- #
+
+## -------------------------------------- Ajustar imagen ---------------------------------------- #
 # Lo mejor de los ensayos anteriores es el mínimo rectángulo contenedor en
 # la figura.
 
-top,bottom,left,right = matlab.expand(Img_gris2,Box) # En realidad no es de
-# matlab pero la puse ahí para tener todo en el mismo archivo
+Img_color = matlab.expand(Img_color,Box)
+Img_BW = matlab.expand(Img_BW,Box)
+# En realidad no es de matlab pero la puse ahí para tener todo en el mismo archivo
 
-Img_texto = cv2.copyMakeBorder(Img_texto, top, bottom, left,
-                                 right, cv2.BORDER_CONSTANT, value = 0)
-Img_gris2 = cv2.copyMakeBorder(Img_gris2, top, bottom, left,
-                                 right, cv2.BORDER_CONSTANT, value = 0)
-
-# Umbralado
-ret,Area_Texto = cv2.threshold(Img_gris2,25,255,cv2.THRESH_OTSU)
-Ancho,Alto = Area_Texto.shape
-
-Contours, _ = cv2.findContours(Area_Texto,cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
-
-for cnt in Contours:
-    area = cv2.contourArea(cnt)
-    # Calcula el area de cada contorno
-    if area > 0.2*Ancho*Alto:
-        # Si el area elegida abarca un 20% de la imagen
-
-        # Envolvente convexa
-        Hull = cv2.convexHull(cnt)
-        Hull = np.resize(Hull,( int( np.size(Hull)/2 ), 2))   # Formateo
-        cv2.drawContours(Img_texto,[Hull],0,(255,0,255),5 )
-        for points in Hull:
-            cv2.circle(Img_texto,(points[0],points[1]),5,(255,0,0),10)
-
-        # Minimo Rectangulo contenedor de la figura
-        Rect = cv2.minAreaRect(cnt)
-        points = cv2.boxPoints(Rect)
-        Box = np.int32(points)
-        print(Box, Box.shape)
-        cv2.drawContours(Img_texto,[Box],0,(0,0,255),5 )
-        # Pasar Box como una lista
-
-# Imprimir resultados para la caja, funca relativamente bien, faltan ajustes
-# print(Bbox)
-# Hull = np.resize(Hull,(np.int( np.size(Hull)/2 ), 2))
-# print(Hull, Hull.shape )
-# matlab.imshow(Img_texto, title = 'Figura ajustada')
-
-# P_1 = get_corners(Img,100,100)
-# A traves de get_corners se obtienen las esquinas automaticamente
-
-# for points in Hull:
-#     cv2.circle(Img_color,(points[0],points[1]),5,(255,0,0),10)
-
-Img_esq = Img_gris2.copy()
-# Img_esq = cv2.drawContours(Img_esq,Box,-1,(255,0,0),25)
-# cv2.polylines(Img_esq, Box, True, (0,0,255),25)
-# matlab.imshow(Img_esq, title = 'Esquinas detectadas')
+# matlab.imshow(Img_color, title = 'Imagen Expandida')
 
 Box = np.float32(Box)
 # Se convierte P_1 a float32
-Box = np.float32([ [2723,334],[832,1155],[1511,2386],[3749,1155] ]) # Valor obtenido a mano
+
+match Img_path:
+    # Se usan valores obtenidos a mano para hacer la transformación de perspectiva.
+    case "foto_1.jpg":
+        # Se elige como primer punto el que tiene menor coordenada 'y' y a partir de ahí se
+        # recorren los puntos en sentido antihorario
+        Box = np.float32([ [2713,360],[829,1170],[1508,2386],[3683,1185] ]) # Hecho
+        # Box = np.float32([ Sup_Der, Sup_Izq, Inf_Izq,Inf_Der ])
+
+    case "foto_2.jpg":
+        Box = np.float32([ [2219,447],[3,792],[196,2067],[2416,1722] ]) # Hecho
+
+    case "foto_3.jpg":
+        Box = np.float32([ [3515,285],[895,398],[576,1750],[4036,1673] ]) # Hecho
+    
+    case "foto_4.jpg":
+        Box = np.float32([ [3344,253],[742,240],[405,1647],[3780,1638] ]) # Hecho
+
+    case "foto_5.jpg":
+        Box = np.float32([ [940,105],[962,2136],[2081,2285],[2068,0] ]) # Hecho
+
+    case "foto_6.jpg":
+        Box = np.float32([ [3546,1244],[1780,158],[883,933],[2741,2309] ]) # Hecho
+
+    case "foto_7.jpg":
+        Box = np.float32([ [2426,269],[873,1673],[1943,2686],[3469,879] ]) # Hecho
 
 Ancho,Alto = 2000,800    # Ancho y alto de la nueva imagen
 
@@ -216,11 +157,44 @@ Matriz = cv2.getPerspectiveTransform(Box,P_2)
 # Este comando crea la matriz para girar la imagen
 # Ambos conjuntos deben ser de tipo np.float32
 
-Img_BW = cv2.copyMakeBorder(Img_BW, top, bottom, left, right, cv2.BORDER_CONSTANT, value = 0)
+
 Transformada = cv2.warpPerspective(Img_BW,Matriz,(Ancho,Alto))
 # Transformada = cv2.warpPerspective(Img_color,Matriz,(Ancho,Alto))
 # Este comando gira la imagen a partir de la matriz obtenida
 
 matlab.imshow(Transformada, title = 'Imagen Transformada')
 
-## --------------------------------------------------------------------- #
+## ---------------------------------------------------------------------------------------------- #
+
+
+## ----------------------------------- Deteccion de palabras ------------------------------------ #
+# Se usa dilatación con distintos kernel para resaltar palabras o párrafos
+# No se dilata para obtener los caracteres
+
+kernel = np.ones((3,3),np.uint8)
+Img_BW = cv2.morphologyEx(Transformada, cv2.MORPH_OPEN, kernel)
+Output = cv2.connectedComponentsWithStats(Img_BW, 8, cv2.CV_32S)
+Img_color,num_labels = matlab.resultados(Output)
+print("Número de caracteres: ", num_labels)
+matlab.imshow(Img_color, title = 'Caracteres detectados')
+
+dil_palabra = 0.25
+Img_BW = matlab.dilatacion_especial(Transformada, Ancho_prom, Alto_prom, dil_palabra)
+Output = cv2.connectedComponentsWithStats(Img_BW, 8, cv2.CV_32S)
+Img_color,num_labels = matlab.resultados(Output)
+print("Número de palabras: ", num_labels)
+matlab.imshow(Img_color, title = 'Palabras detectadas')
+
+dil_parrafos = 0.7
+Img_BW = matlab.dilatacion_especial(Transformada, Ancho_prom, Alto_prom, dil_parrafos)
+Output = cv2.connectedComponentsWithStats(Img_BW, 8, cv2.CV_32S)
+Img_color,num_labels = matlab.resultados(Output)
+print("Número de párrafos: ", num_labels)
+matlab.imshow(Img_color, title = 'Parrafos detectados')
+
+matlab.resultados_finales()
+# Los resultados con la imagen transformada son mejores que con la imagen
+# original. Se tiene mejor diferenciación de palabras y menos errores
+# (aunque podría decirse que son la misma cosa)
+
+## ---------------------------------------------------------------------------------------------- #
--- matlab.py
+++ matlab.py
@@ -6,7 +6,11 @@ import matplotlib.pyplot as plt
 # Gracias a Murtaza's Workshop - Robotics and AI, sin su ayuda este
 # archivo quiza no existiria
 
-## --------------------------- Funcion imshow -------------------------- #
+## ---------------------------------------- Funcion imshow -------------------------------------- #
+# Equivalente a la función imshow de matlab
+# Parametros de entrada:
+#   Img: Imagen a mostrar
+#   Title: Título a mostrar
 def imshow(Img,title = None):
     plt.figure()
     if len(Img.shape) == 3:
@@ -19,12 +23,13 @@ def imshow(Img,title = None):
     plt.show()
     # plt.show() espera a que la imagen se cierre para proseguir
 
-## --------------------------------------------------------------------- #
+## ---------------------------------------------------------------------------------------------- #
 
 
 
-## ----------------------- Funcion imclearborder ----------------------- #
-# Funcion imclearborder (equivalente a imclearborder de MATLAB)
+## ------------------------------------ Funcion imclearborder ----------------------------------- #
+# Funcion imclearborder: (equivalente a imclearborder de MATLAB), elimina los bordes blancos en
+# una imagen binaria.
 def imclearborder(Img_BW):
     # Es importante insertar una imagen blanco y negro, pero voy a
     # tratar de adaptarlo asi no me complico la vida
@@ -65,11 +70,11 @@ def imclearborder(Img_BW):
 
     return Img_clear
 
-## --------------------------------------------------------------------- #
+## ---------------------------------------------------------------------------------------------- #
 
 
-## --------------------------- Funcion imfill -------------------------- #
-# Funcion equivalente a imfill de MATLAB
+## ---------------------------------------- Funcion imfill -------------------------------------- #
+# Funcion equivalente a imfill de MATLAB. Llena los huecos en cualquier imagen binaria
 def imfill(Img):
 # Gracias a Satya Mallick por el script
 # https://learnopencv.com/filling-holes-in-an-image-using-opencv-python-c/
@@ -93,31 +98,40 @@ def imfill(Img):
     Salida = Img | Img_floodfill_inv
     return Salida
 
-## --------------------------------------------------------------------- #
+## ---------------------------------------------------------------------------------------------- #
 
 
 
-## ------------------------- Funcion label2rgb ------------------------- #
-# Funcion equivalente a label2rgb de MATLAB
-def label2rgb(labels,N_label,color_fondo = (0,0,0),colormap = 2):
+## -------------------------------------- Funcion label2rgb ------------------------------------- #
+# Funcion equivalente a label2rgb de MATLAB. A partir de la matriz de etiquetas, forma una imagen
+# RGB. Se necesita el número de etiquetas 
+
+# Parámetros de entrada:
+#   labels: Matriz de etiquetas correspondiente a la imagen
+#   N_label: Número de etiquetas en la matriz
+
+def label2rgb(labels,N_label):
     # Mascara logica con los pixeles correspondientes al fondo
     Fondo = labels == 0
 
-    # Convierte la matriz etiqueta a RGB
+    # Se debe convertir la matriz para poder aplicarle el mapa de color
     labels = np.uint8( (255*labels)/N_label )
-    Img_color = cv2.applyColorMap(labels, colormap)
+    # Convierte la matriz etiqueta a RGB
+    Img_color = cv2.applyColorMap(labels, 4)
 
     # Usa la mascara Fondo para cambiar el color de fondo
-    Img_color[Fondo] = color_fondo
+    Img_color[Fondo] = (0,0,0)
 
     return Img_color
 
-## --------------------------------------------------------------------- #
+## ---------------------------------------------------------------------------------------------- #
 
 
-## ---------------------- Funcion Deteccion_texto ---------------------- #
-# Funcion Deteccion_texto
-def dilatacion(Imagen_BW, Ancho_prom, Alto_prom, proporcion):
+## --------------------------------- Funcion dilatacion_especial -------------------------------- #
+# Efectúa una dilatación usando los valores promedio de las letras, y una constante de proporción
+# que depende de si se quiere agrupar palabras, párrafos, o si se quiere obtener la región de
+# interés del texto completo.
+def dilatacion_especial(Imagen_BW, Ancho_prom, Alto_prom, proporcion):
     Imagen_BW = imfill(Imagen_BW)
     
     Ancho_kernel = int( Ancho_prom*proporcion )
@@ -128,20 +142,18 @@ def dilatacion(Imagen_BW, Ancho_prom, Alto_prom, proporcion):
     Deteccion = imfill(Deteccion)
     # El tercer argumento es opcional y por defecto es 1
 
-    Output = cv2.connectedComponentsWithStats(Deteccion, 8, cv2.CV_32S)
+    return Deteccion
 
-    # Resultados
-    num_labels =    Output[0]   # Cantidad de elementos
-    labels =        Output[1]   # Matriz con etiquetas
+## ---------------------------------------------------------------------------------------------- #
 
-    # Coloreamos los elementos
-    Img_color = label2rgb(labels,num_labels)
-    return Img_color
 
-## --------------------------------------------------------------------- #
+## --------------------------------------- Función expand --------------------------------------- #
+# Expande la imagen para que contenga al mínimo rectángulo que contiene al texto.
 
-## -------------------------- Función expand --------------------------- #
-# Expande la imagen así entra el rectángulo que contiene al
+# Parámetros de entrada:
+#   Img: La imagen en cuestión
+#   Box: Cordenadas del rectángulo
+
 def expand(Img,Box):
     Dim = list(reversed(Img.shape[:2]))
 
@@ -158,7 +170,115 @@ def expand(Img,Box):
         if Box[i][1] > Dim[1] and abs(Box[i][1]) > Bottom:
             Bottom = abs(Box[i][1])
 
-    print (Top,Bottom,Left,Right)
-    return Top,Bottom,Left,Right
+    Img_out = cv2.copyMakeBorder(Img, Top, Bottom, Left, Right, cv2.BORDER_CONSTANT, value = 0)
+    return Img_out
 
-## --------------------------------------------------------------------- #
+## ---------------------------------------------------------------------------------------------- #
+
+
+## --------------------------------- Aproximar región de interes -------------------------------- #
+# Aproxima el contorno del texto y devuelve los 4 puntos de un polígono irregular que lo contiene.
+# Esto es generado por chatgpt y me dió un buen par de ideas, pero aún no está probado.
+
+# Parámetros de entrada:
+#   Img_path: Path de la imagen. Se abre la imagen dentro de la función
+# Parámetros de salida:
+#   approx_polygons: Coordenadas del polígono aproximado que contiene al texto
+
+def approx_contour(Img_path) -> list:
+    image = cv2.imread(Img_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_image, 25,255)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    approx_polygons = []
+    for contour in contours:
+        epsilon = 0.04 * cv2.arcLength(contour, True)
+        # El epsilon debe ajustarse en función de la precisión requerida
+        polygon = cv2.approxPolyDP(contour, epsilon, True)
+        if len(polygon) == 4:  # Filtra los polígonos con 4 puntos
+            approx_polygons.append(polygon) # Agregar elementos
+    cv2.drawContours(image, approx_polygons, -1, (0, 255, 0), 2) 
+    # Green color and line thickness 2
+    return approx_polygons
+
+## ---------------------------------------------------------------------------------------------- #
+
+## ----------------------------- Dimensiones promedio de caracteres ----------------------------- #
+# Para efectuar la dilatación especial, primero deben obtenerse las dimensiones promedio de los
+# caracteres de la imagen. Para ello se usa ésta función, tomando como parámetro de entrada las
+# propiedades obtenidas a partir del algoritmo de componentes conectadas.
+
+# Parámetros de entrada:
+#   Conn_components: Resultado obtenido de la función cv2.connectedComponentsWithStats aplicado
+#   a la imagen deseada
+
+def dim_promedio(Conn_components) -> tuple:
+    num_labels =    Conn_components[0]   # Cantidad de elementos
+    stats =         Conn_components[2]   # Matriz de stats
+
+    Suma_Alto = 0;    Suma_Ancho = 0    # Inicializo contadores
+
+    # Agregamos Bounding Box
+    for i in range(1,num_labels):
+        Bbox = stats[i,]
+        # Bbox[2] : Ancho
+        # Bbox[3] : Alto
+
+        Suma_Ancho = Suma_Ancho + Bbox[2]
+        Suma_Alto = Suma_Alto + Bbox[3]
+
+    # TODO puede estar mal, considerando siempre una etiqueta de más, la del fondo
+    # El return es el ancho y alto promedio de letras.
+    return Suma_Ancho/num_labels, Suma_Alto/num_labels
+
+## ---------------------------------------------------------------------------------------------- #
+
+## ----------------------------------- Componentes conectadas ----------------------------------- #
+def resultados(Conn_components):
+
+    num_labels =    Conn_components[0]   # Cantidad de elementos
+    labels =        Conn_components[1]   # Matriz con etiquetas
+    stats =         Conn_components[2]   # Matriz de stats
+
+    Img_salida = label2rgb(labels,num_labels)
+
+    # Agregamos Bounding Box
+    for i in range(1,num_labels):
+        Bbox = stats[i,]
+        # Bbox[0] : Coordenada x punto superior izquierdo
+        # Bbox[1] : Coordenada y punto superior izquierdo
+        # Bbox[2] : Ancho
+        # Bbox[3] : Alto
+
+        cv2.rectangle(Img_salida, (Bbox[0], Bbox[1]), (Bbox[0]+Bbox[2],
+            Bbox[1]+Bbox[3]), (255,255,255), 2)
+
+    cant_elementos = (num_labels - 1) # En num_labels se cuenta el fondo también
+    return Img_salida, cant_elementos
+
+## ---------------------------------------------------------------------------------------------- #
+
+
+## -------------------------------------- Analisis de texto ------------------------------------- #
+def resultados_finales():
+    str = 'Este es el texto que debe ser leido de manera automática. \n'
+    str = str + 'El algoritmo implementado debe ser capaz de detectar todas las palabras. '
+    str = str + 'Para evitar que este problema sea muy complicado '
+    str = str + 'todos los caracteres estan en mayusculas y los acentos fueron omitidos. \n'
+    str = str + 'Recuerde que también debe detectar los parrafos presentes en esta imagen. '
+    str = str + 'Resalte mediante un recuadro de color los parrafos presentes en el texto. Como'
+    str = str + ' objetivo opcional se propone detectar las oraciones presentes en el texto. \n'
+    str = str + 'Para ello deberá tambien detectar los puntos que separan cada oracion. '
+    str = str + 'En caso de lograrlo subraye cada oracion de texto analizado. \n'
+    
+    char_total = len(str)
+    espacios = str.count(' ')
+    puntos = str.count('.')
+    parrafos = str.count('\n')
+    caracteres = char_total - espacios - puntos - parrafos
+    palabras = espacios # Modifique un poco el texto para tener un espacio por palabra
+    print("Caracteres =", caracteres)
+    print("Palabras =", palabras)
+    print("Parrados =", parrafos)
+
+## ---------------------------------------------------------------------------------------------- #
